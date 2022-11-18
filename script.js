2990c1a791453b63822bf9663058a4dfc8f9862e
rubeans <rubensglf@gmail.com>
2022-11-17 23:35:23 -0300

    fix it to not display operators in a roll

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ const operatorsBtns = document.querySelectorAll('.operator')
 const equalsBtn = document.querySelector('.equals-btn')
 // FLEXIBLE VARIABLES
 let currentNumber;
-let operator;
-let prevOp;
+let currentOperator;
+let prevOperator;
 let combinedValue;
 let prevNum;
 let lastNum;
@@ -39,7 +39,7 @@ function handleClearBtn() {
     clearBtn.addEventListener('click', () => {
         if (output.textContent != 'Made by Rubens') {
             combinedValue = ''
-            operator = ''
+            currentOperator = ''
             output.textContent = ''
         } else {
             console.error('[ERROR] Not possible to clear the dev name.')
@@ -56,14 +56,14 @@ function handleDeleteBtn() {
 }
 
 // WHEN NUMBER BUTTONS IS CLICKED
-function handleNumbersBtn() {
+function handleNumberBtns() {
     numberBtns.forEach(btn => btn.addEventListener('click', () => {
         currentNumber = btn.value
         combinedValue == undefined ? combinedValue = currentNumber : combinedValue += currentNumber;
         if (output.textContent == 'Made by Rubens' || output.textContent.length == 0) {
             output.textContent = ''
         } else if (output.textContent == '-') {
-            combinedValue = operator += currentNumber
+            combinedValue = currentOperator += currentNumber
         };
         output.style.fontSize = '2.5rem'
         output.style.alignSelf = 'flex-end'
@@ -73,51 +73,56 @@ function handleNumbersBtn() {
 }
 
 // WHEN OPERATOR BUTTONS IS CLICKED
-function handleOperatorsBtn() {
+function handleOperatorBtns() {
     operatorsBtns.forEach(btn => btn.addEventListener('click', () => {
-        operator = btn.value
-        if (operator != '-' && output.textContent == 'Made by Rubens' || output.textContent == '') {
-            console.error(`[ERROR] Not possible to start with '${operator}' operator.`)
+        currentOperator = btn.value
+        if (currentOperator != '-' && output.textContent == 'Made by Rubens' || output.textContent == '' && currentOperator != '-') {
+            console.error(`[ERROR] Not possible to start with '${currentOperator}' currentOperator.`)
             return;
-        } else if (operator == '-' && output.textContent == 'Made by Rubens') {
+        } else if (currentOperator == '-' && output.textContent == 'Made by Rubens') {
             output.style.alignSelf = 'flex-end'
-            output.textContent = operator
+            output.textContent = currentOperator
             return;
-        } else if (output.textContent.includes(prevOp)) {
-            getPrevOp(prevOp)
-            combinedValue = operate(prevOp, prevNum, lastNum)
+        } else if (output.textContent.includes(prevOperator)) {
+            // change the operator in the middle of the operation
+            if (currentOperator === prevOperator && currentOperator === output.textContent.slice(-1) || prevOperator === output.textContent.slice(-1)) {
+                alert('Changing the operator in the middle of the operation can also change the final value.')
+                return
+            }
+            getPrevOp(prevOperator)
+            combinedValue = operate(prevOperator, prevNum, lastNum)
             output.textContent = combinedValue
         }
         prevNum = combinedValue
         combinedValue = ''
-        output.textContent += operator
-        prevOp = operator
+        output.textContent += currentOperator
+        prevOperator = currentOperator
     }))
 }
 
 // WHEN EQUALS BUTTON IS PRESSED
 function handleEqualsBtn() {
     equalsBtn.addEventListener('click', () => {
-        getOperator(operator)
-        combinedValue = operate(operator, prevNum, lastNum)
+        getCurrentOp(currentOperator)
+        combinedValue = operate(currentOperator, prevNum, lastNum)
         output.textContent = combinedValue
     })
 }
 
 // CONVERT OPERATORS SYMBOLS TO IT'S NAME
-function getOperator(op) {
+function getCurrentOp(op) {
     switch (op) {
         case '÷':
-            operator = divide
+            currentOperator = divide
             break;
         case '×':
-            operator = multiply
+            currentOperator = multiply
             break;
         case '-':
-            operator = subtract
+            currentOperator = subtract
             break;
         case '+':
-            operator = add
+            currentOperator = add
             break;
     }
 }
@@ -126,16 +131,16 @@ function getOperator(op) {
 function getPrevOp(op) {
     switch (op) {
         case '÷':
-            prevOp = divide
+            prevOperator = divide
             break;
         case '×':
-            prevOp = multiply
+            prevOperator = multiply
             break;
         case '-':
-            prevOp = subtract
+            prevOperator = subtract
             break;
         case '+':
-            prevOp = add
+            prevOperator = add
             break;
     }
 }
@@ -144,13 +149,13 @@ function getPrevOp(op) {
 window.onload = () => {
     handleClearBtn()
     handleDeleteBtn()
-    handleNumbersBtn()
-    handleOperatorsBtn()
+    handleNumberBtns()
+    handleOperatorBtns()
     handleEqualsBtn()
 }
 
 /* TODO
-    fix it to not let display more than one operator and dot in a roll;
+    fix it to not let display more than a dot in a roll;
     fix delete button to actually delete an item not just the text;
     handle floats numbers to just display 1 numbers after the dot;
 */
